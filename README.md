4bcd07a67c2410db0073ab2f2ba1fb35e11e11b7
Thushani Jayasekera <thushanij.18@cse.mrt.ac.lk>
2024-09-26 01:20:37 +0530

    Add changes to react client

--- README.md
+++ README.md
@@ -1,21 +1,3 @@
 # Chatroom
 WebSocket-based chatroom application with Node.js + Express.js backend
 
-![Cover](https://i.imgur.com/0I5RSGE.png)
-
-## Prerequisites
-
-Before you can run this program, ensure that you have the following software installed and functional:
-* Node.js v10.19.0 or newer
-* All necessary Node package dependencies as listed in [package-lock.json](package-lock.json). (note: you can install all dependencies by running the command `npm install`.
- 
-## Running
-1. Download the source code from this repository.
-2. In the project root, run the command `npm start` to start the server.
-3. Open a browser, type 'localhost:3000' into the url bar, and press enter.
-4. Set the username for the new client by clicking 'Choose a nickname' in the top-right corner of the page. Then press the connect button at the bottom of the page to connect to the websocket server and be placed in a room.
-5. To talk with another instance of the client, repeat steps 3-4 in a new tab, window, or browser.
-
-## Authors
-
-* [**Ethan Genser**](https://github.com/Ethan-Genser) - *Creator*
---
+++ chat-app/src/App.tsx
@@ -0,0 +1,82 @@
+import { AuthProvider, useAuthContext } from "@asgardeo/auth-react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Contact from "./pages/ChatRoom";
+import PageNotFound from "./pages/PageNotFound";
+import "./App.css";
+import Unauthenticated from "./pages/Unauthenticated";
+import { ReactNode, useEffect, useState } from "react";
+import React from "react";
+import ChatRoom from "./pages/ChatRoom";
+
+const authConfig = {
+  signInRedirectURL: "http://localhost:3000",
+  signOutRedirectURL: "http://localhost:3000",
+  clientID: "owbVZdTq_x6Sfqj5RaX2CW4_CQMa",
+  baseUrl: "https://api.asgardeo.io/t/thushaniprivate",
+  scope: ["openid", "profile", "email"],
+};
+
+const AppContent = () => {
+  const ProtectedRoute = ({ children }: { children: ReactNode }) => {
+    const { state } = useAuthContext();
+
+    if (!state.isAuthenticated) {
+      return <Unauthenticated />;
+    }
+
+    return children;
+  };
+
+  const { getBasicUserInfo, getAccessToken } = useAuthContext();
+  const [token, setToken] = useState("");
+  const [email, setEmail] = useState("");
+
+  useEffect(() => {
+    getAccessToken()
+      .then((accessToken) => {
+        setToken(accessToken);
+      })
+      .catch((error) => {
+        console.log(error, "error");
+      });
+
+    getBasicUserInfo()
+      .then((basicUserDetails) => {
+        console.log(basicUserDetails);
+        setEmail(basicUserDetails.email ? basicUserDetails.email : "");
+      })
+      .catch((error) => {
+        console.log(error, "error");
+      });
+  }, []);
+
+  return (
+    <Routes>
+      <Route
+        path="/contact"
+        element={
+          <ProtectedRoute>
+            <ChatRoom
+              chatroomId={"Personal"}
+              bearerAccessToken={token}
+              nickname={email}
+            />
+          </ProtectedRoute>
+        }
+      />
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<PageNotFound />} />
+    </Routes>
+  );
+};
+
+const App = () => (
+  <AuthProvider config={authConfig}>
+    <Router>
+      <AppContent />
+    </Router>
+  </AuthProvider>
+);
+
+export default App;
